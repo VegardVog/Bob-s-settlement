b4025b1c3727be2a464e7318ffc8c6c4022aab0c
Vegard Vognstolen <vegard.bruas.vognstolen@no.experis.com>
2024-03-12 11:10:31 +0100

    Create Spring project

---
+++ src/test/java/com/booleanUk/Bob/s/Settlement/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.booleanUk.Bob.s.Settlement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
