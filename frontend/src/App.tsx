d1b2c41ca40a91a185311a2e01b04a968545e4d3
Vegard Vognstolen <vegard.bruas.vognstolen@no.experis.com>
2024-03-15 15:50:04 +0100

    #21 Add navbar

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,14 +3,17 @@ import "./App.css";
 import { Route, Routes } from "react-router-dom";
 import Signup from "./pages/auth/Signup";
 import Login from "./pages/auth/Login";
+import Dashboard from "./pages/Dashboard";
+import Menu from "./components/Menu";
 
 function App() {
   return (
     <>
+     <Menu />
     <Routes>
-    
         <Route path="/signup" element={<Signup />}/>
         <Route path="/login" element={<Login />}/>
+        <Route path="/" element={<Dashboard />}/>
 
     </Routes>
     </>
