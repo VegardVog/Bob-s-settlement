093af1f27f65273f0f08ce3ad6890d18912fd316
Vegard Vognstolen <vegard.bruas.vognstolen@no.experis.com>
2024-03-18 11:21:03 +0100

    Add userContext but need Jwt http cookies

--- frontend/src/components/Menu.tsx
+++ frontend/src/components/Menu.tsx
@@ -1,20 +1,29 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import "../styles/menu.css";
 import { Link } from 'react-router-dom';
+import { UserContext, UserTypes } from '../contextAPI/User';
 
 const Menu = () => {
+
+
+    const {loggedIn} = useContext(UserContext) as UserTypes;
+
+
   return (
     <div className='menu-container'>
         <ul className='menu-ul'>
             <Link to={"/"} style={{ textDecoration: 'none' }}>
                 <li className='menu-li' >Home</li>
             </Link>
-            <Link to={"/login"} style={{ textDecoration: 'none' }}>
+
+            {!loggedIn} {
+            <><Link to={"/login"} style={{ textDecoration: 'none' }}>
                 <li className='menu-li'>Login</li>
-            </Link>
-            <Link to={"/signup"} style={{ textDecoration: 'none' }}>
+            </Link><Link to={"/signup"} style={{ textDecoration: 'none' }}>
                 <li className='menu-li'>Signup</li>
-            </Link>
+            </Link></>
+            }
+
 
         </ul>
     </div>
